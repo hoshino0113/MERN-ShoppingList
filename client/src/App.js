b67d1e308b269a7835609fa6ed69c19d71ecc712
Hoshino <x43zhong@uwaterloo.ca>
2024-12-01 17:27:46 -0500

    Implemented add, delete using redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
+import { Container } from 'reactstrap';
+
 import {Provider, provider} from 'react-redux';
 import store from './store';
 
@@ -13,7 +16,10 @@ function App() {
     <Provider store={store}>
     <div className="App">
       <AppNavbar />
-      <ShoppingList/>
+      <Container>
+        <ItemModal />
+        <ShoppingList />
+      </Container>
     </div>
     </Provider>
   );
---
+++ client/src/components/itemModal.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+}from 'reactstrap';
+import { v4 as uuidv4 } from 'uuid';
+
+import {connect} from 'react-redux';
+import {addItem} from '../actions/itemActions'
+
+
+class ItemModal extends Component {
+    state = {
+        modal: false, // off by default
+        name: ''
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        }); 
+    }
+
+    onChange = (e) => { 
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault(); //prevent the form from submitting
+        const newItem = {
+            id : uuidv4(), //id determined by mongoDB
+            name: this.state.name
+        }
+
+        this.props.addItem(newItem) //add item via addItem action
+
+        this.toggle();//close the modal
+    }
+
+
+    render(){
+        return (
+            <div><Button color='dark' 
+                         style={{marginBottom : '2rem'}} 
+                         onClick={this.toggle}>
+                         Add Item
+                </Button>
+                <Modal
+                isOpen={this.state.modal}
+                toggle={this.toggle}
+                >
+                    <ModalHeader toggle={this.toggle}>
+                        Add to my shopping list!
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.onSubmit}>
+                            <FormGroup>
+                                <Label for="item">Item</Label>
+                                <Input type = "text" name="name" id="item" 
+                                       placeholder="Add shopping item" onChange={this.onChange} />
+                                <Button color = "warning" style={{marginTop: '2rem'}}>
+                                    Add Item!
+                                </Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    item: state.item
+});
+
+export default connect(mapStateToProps, {addItem})(ItemModal);
