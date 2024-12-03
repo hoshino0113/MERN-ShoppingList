5888aafa05fafe035da07d01a3bdf0fd73e000ef
Hoshino <chocolatealan0601@gmail.com>
2024-12-03 18:26:52 -0500

    add post deply

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser"); //enable us to get the body of the request
-
+const path = require('path')
 const app = express();
 const items = require('./routes/api/items');
 
@@ -18,6 +18,13 @@ mongoose.connect(db)
 
 // Use routes
     app.use('/api/items', items); // anything goes to the 'api/items' location should refers to items variable
+
+    if (process.env.NODE_ENV === 'production'){
+        app.use(express.static('client/build'));
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        })
+    }
 
     const port = process.env.port || 6000; //Define the port number, can either be the sys port or port 3000
     app.listen(port, () => console.log(`Listening to Port ${port}`)) //The application is listening to Port
