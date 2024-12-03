e6b4c5bfa3ab7db2dbdf737f2f683cb23d27f12e
Hoshino <chocolatealan0601@gmail.com>
2024-12-03 17:06:53 -0500

    remove auto post deploy

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ mongoose.connect(db)
     .catch(err => console.log(err));
 
 // Use routes
-    app.use('/api/items', items); // anything goes to the 'api/items' location should refers to items variable
-    if (process.env.NODE_ENV == 'production'){
-        app.use(express.static('client/build'));
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-        })
-    }
+
 
     const port = process.env.port || 6000; //Define the port number, can either be the sys port or port 3000
     app.listen(port, () => console.log(`Listening to Port ${port}`)) //The application is listening to Port
